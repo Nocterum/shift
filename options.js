8fc73e89262420047fbcd2b26b67b43716027352
Nocterum <master.of.colours@yandex.ru>
2024-05-22 14:26:55 +0300

    изменил метод добавления кнопки к сообщению в момент редактирования8

--- options.js
+++ options.js
@@ -1,4 +1,15 @@
 
+const MSK_fromToSendOptionsReply_markup = JSON.stringify( {
+    inline_keyboard: [
+        [ { text: 'Глаголева', callback_data: 'fromToSend=Глаголева' } ],
+        [ { text: 'Патрики', callback_data: 'fromToSend=Патрики' } ],
+        [ { text: 'Арт-плей', callback_data: 'fromToSend=Арт-плей' } ],
+        [ { text: 'Рублёвка', callback_data: 'fromToSend=Рублёвка' } ],
+        [ { text: 'Офис', callback_data: 'fromToSend=Офис' } ],
+        [ { text: 'Центральный склад', callback_data: 'fromToSend=Центральный склад' } ],
+    ]
+});
+
 module.exports = {
 
     mainMenuUsersOptions: {
@@ -89,7 +100,7 @@ module.exports = {
         })
     },
 
-    MSK_fromToSendReply_markup: JSON.stringify( {
+    MSK_fromToSendOptionsReply_markup: JSON.stringify( {
         inline_keyboard: [
             [ { text: 'Глаголева', callback_data: 'fromToSend=Глаголева' } ],
             [ { text: 'Патрики', callback_data: 'fromToSend=Патрики' } ],
